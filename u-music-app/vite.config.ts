7267f542c002f1063fb9329683bc60bd76d1df9d
snowdy78 <themrleoner12@mail.ru>
2025-01-14 16:42:28 +0700

    configuration changed

--- u-music-app/vite.config.ts
+++ u-music-app/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@css': fileURLToPath(new URL('./src/css', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url))
+    }
+  }
 })
