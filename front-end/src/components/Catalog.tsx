f3c505ea90c086d84798bc21fecb98bb8f94337a
snowdy78 <themrleoner12@mail.ru>
2025-01-16 12:32:19 +0700

    Catalog opening fix

--- front-end/src/components/Catalog.tsx
+++ front-end/src/components/Catalog.tsx
@@ -1,28 +1,26 @@
 import '@css/Catalog.css'
 import { PropsWithChildren, useRef, useState } from "react";
-import { useClickOutside } from "../hooks/useClickOutside.ts";
 export type CatalogType = PropsWithChildren & {};
 
 export function Catalog({ children }: CatalogType) {
     const [isOpen, setIsOpen] = useState(false);
     const button = useRef<HTMLButtonElement>(null);
     const list = useRef<HTMLDivElement>(null);
-    useClickOutside(button, event => {
-        if (button.current
-            && event.target instanceof Node
-            && !button.current.contains(event.target))
-            setIsOpen(false)
-    });
     return (
         <div className='catalog'>
-            <button ref={button} className='catalog-dropdown-list' onClick={() => setIsOpen(!isOpen)}>
+            <button 
+                ref={button} 
+                className='catalog-dropdown-list' 
+                onBlur={() => setIsOpen(false)} 
+                onClick={() => setIsOpen(!isOpen)}
+            >
                 <img src="./src/assets/icons/list.svg" className='icon' alt="" />
                 Каталог
             </button>
             <div
                 ref={list}
                 className='catalog-dropdown-list__open'
-                style={isOpen ? { maxHeight: '300px' } : { maxHeight: '0' }}
+                style={isOpen ? { scale: '1 1' } : { }}
             >
                 <div className='catalog-dropdown-list__items' style={isOpen ? { opacity: '1' } : { opacity: '0' }}>
                     {children}
